548e039216df2e07f5de7fe275c686cf6e460d03
Clement Murphy <clemmurphy@gmail.com>
2021-12-07 15:30:25 +0000

    Updated services, jumbotron and company description content

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const quote = document.getElementById('quote-text')
   const companyDescriptionText = document.getElementById('company-span')
+
+  const quotes = [
+    'My live performance, it just comes from feeling an energy and emotion from the crowd. - Wiz Khalifa',
+    'I like a performance, a live performance, so I like little mistakes because that\'s what makes perfect - the mistakes. - Jorja Smith',
+    'It always seems impossible until it’s done. – Nelson Mandela',
+    'An event is not over until everyone is tired of talking about it. – Mason Cooley',
+    'The first rule for choosing vendors is to avoid those who suggest your budget isn’t sufficient. The planning process should be about taking your ideas and making them work. – Mindy Weiss',
+    'If you ever need anything please don’t hesitate to ask someone else first. — Kurt Cobain'
+  ]
+
+  quote.innerText = quotes[Math.floor(Math.random() * quotes.length)]
 
   const companyDescriptions = [
     'production',
-    'very very good',
-    'very good and very cheap',
-    'come and have a look',
-    'one pound fish'
+    'live events',
+    'video',
+    'conferencing',
+    'VMix',
+    'podcasting',
+    'audio',
+    'editing',
+    'equipment hire'
   ]
 
   const changeCompanyText = setInterval(() => {
